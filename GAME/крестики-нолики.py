c49955c75cd4b14cd3cf9a6914002310a3766bf7
Nikita-Postnov <nikita.postnov1@gmail.com>
2024-03-15 15:02:25 +0300

    крестики-нолики

---
+++ GAME/крестики-нолики.py
@@ -0,0 +1,52 @@
+# Пример игры "Крестики-нолики" с использованием библиотеки tkinter для создания графического интерфейса на Python
+
+import tkinter as tk
+from tkinter import messagebox
+
+class TicTacToe:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Крестики-нолики")
+        self.current_player = "X"
+        self.board = [" " for _ in range(9)]
+        self.buttons = []
+        self.create_board()
+
+    def create_board(self):
+        for i in range(3):
+            for j in range(3):
+                button = tk.Button(self.root, text="", font=("Arial", 20), width=5, height=2,
+                                  command=lambda i=i, j=j: self.on_button_click(i, j))
+                button.grid(row=i, column=j, padx=5, pady=5)
+                self.buttons.append(button)
+
+    def on_button_click(self, i, j):
+        index = i * 3 + j
+        if self.board[index] == " ":
+            self.board[index] = self.current_player
+            self.buttons[index].config(text=self.current_player)
+            if self.check_winner():
+                messagebox.showinfo("Победа!", f"Игрок {self.current_player} победил!")
+                self.reset_game()
+            elif " " not in self.board:
+                messagebox.showinfo("Ничья", "Игра окончена ничьей.")
+                self.reset_game()
+            else:
+                self.current_player = "O" if self.current_player == "X" else "X"
+
+    def check_winner(self):
+        winning_combinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+        for combo in winning_combinations:
+            if self.board[combo[0]] == self.board[combo[1]] == self.board[combo[2]] != " ":
+                return True
+        return False
+
+    def reset_game(self):
+        self.board = [" " for _ in range(9)]
+        for button in self.buttons:
+            button.config(text="")
+        self.current_player = "X"
+
+root = tk.Tk()
+game = TicTacToe(root)
+root.mainloop()
