7826e5501a6bb924326cc05bbb48fd6d01434cf9
Nikita-Postnov <nikita.postnov1@gmail.com>
2024-03-19 09:51:56 +0300

    main

---
+++ C++/CODE/if_ese.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+short int a, b;
+cin>>a;
+cin>>b;
+if(a>b){
+    cout<<"a is greater than b"<<endl;
+    }
+else if (b>a)
+{
+    cout<<"b is greater than a"<<endl;
+}
+else
+{
+    cout<<"a and b are equal"<<endl;
+}
+return 0;
+}
