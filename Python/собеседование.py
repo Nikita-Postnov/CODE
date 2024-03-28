044008cc3f57afbc2023220c604d3818c52ec329
Nikita-Postnov <nikita.postnov1@gmail.com>
2024-03-28 11:44:34 +0300

    change

--- Python/собеседование.py
+++ Python/собеседование.py
@@ -1,26 +1,24 @@
-import unittest
 import math
 
-class Interview:
-    @staticmethod
+class GeometryCalculator:
+
     def circle_area(radius):
+        if radius < 0:
+            raise ValueError("Радиус не может быть отрицательным")
         return math.pi * radius ** 2
 
-    @staticmethod
     def triangle_area(side1, side2, side3):
+        if side1 < 0 or side2 < 0 or side3 < 0:
+            raise ValueError("Длины сторон должны быть положительными")
         s = (side1 + side2 + side3) / 2
         return math.sqrt(s * (s - side1) * (s - side2) * (s - side3))
 
-class TestGeometryCalculator(unittest.TestCase):
-    def test_circle_area(self):
-        self.assertAlmostEqual(Interview.circle_area(1), math.pi)
-        self.assertAlmostEqual(Interview.circle_area(0), 0)
-        self.assertAlmostEqual(Interview.circle_area(5), 25 * math.pi)
+radius = float(input("Введите радиус круга: "))
+circle_area = GeometryCalculator.circle_area(radius)
+print("Площадь круга с радиусом", radius, ":", circle_area)
 
-    def test_triangle_area(self):
-        self.assertAlmostEqual(Interview.triangle_area(3, 4, 5), 6)
-        self.assertAlmostEqual(Interview.triangle_area(6, 8, 10), 24)
-        self.assertAlmostEqual(Interview.triangle_area(5, 5, 5), 10.825317547305485)
-
-if __name__ == '__main__':
-    unittest.main()
+side1 = float(input("Введите длину первой стороны треугольника: "))
+side2 = float(input("Введите длину второй стороны треугольника: "))
+side3 = float(input("Введите длину третьей стороны треугольника: "))
+triangle_area = GeometryCalculator.triangle_area(side1, side2, side3)
+print("Площадь треугольника: ", triangle_area)
