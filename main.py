72cde06d8246ae35063c312e15a34379e2d62b33
Nikita_Postnov <117923446+Nikita-Postnov@users.noreply.github.com>
2025-03-21 17:42:47 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+from password_generator_app import PasswordGeneratorApp
+
+
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = PasswordGeneratorApp(root)
+    root.mainloop()
