cd41a2502a8e9efb82dabbbb26ecca3b09ed4e6a
Nikita_Postnov <117923446+Nikita-Postnov@users.noreply.github.com>
2025-03-22 19:05:13 +0300

    Add files via upload

---
+++ passwords_generator/test/main.py
@@ -0,0 +1,9 @@
+import pyperclip
+import tkinter as tk
+from password_generator_app import PasswordGeneratorApp
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = PasswordGeneratorApp(root)
+    root.mainloop()
