ec942ee33c87fe7e5b0281d3b1ada8fcddbe1e77
Nikita_Postnov <117923446+Nikita-Postnov@users.noreply.github.com>
2025-03-22 19:22:19 +0300

    Delete passwords_generator/test/main.py

--- passwords_generator/test/main.py
+++
@@ -1,9 +0,0 @@
-import pyperclip
-import tkinter as tk
-from password_generator_app import PasswordGeneratorApp
-
-
-if __name__ == "__main__":
-    root = tk.Tk()
-    app = PasswordGeneratorApp(root)
-    root.mainloop()
